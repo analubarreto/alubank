2d0369350c76b3a9f13aef5af9f278b111272426
analubarreto <analuibm@gmail.com>
2023-07-17 16:27:39 -0300

    finished header

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:alubank/components/sections/header.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: <Widget>[
+          Header(),
+        ],
+      ),
+    );
   }
 }
